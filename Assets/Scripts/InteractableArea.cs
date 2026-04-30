09566817a4597d66b637773a389b01a147cd5f54
AviFeder-byte <elduritoo123@gmail.com>
2026-04-30 16:15:04 -0300

    Initial commit

---
+++ Assets/Scripts/InteractableArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableArea : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    { 
+        if(hit.gameObject.CompareTag("Pickable")){ 
+            Destroy(hit.gameObject);
+        }
+    }
+}
