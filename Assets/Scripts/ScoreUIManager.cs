7ecda42123b9775934f128c67a456ab167f5b246
AviFeder-byte <elduritoo123@gmail.com>
2026-05-06 09:07:00 -0300

    cdscsxz

---
+++ Assets/Scripts/ScoreUIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreUIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtScore;
+    public int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtScore.text="0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
